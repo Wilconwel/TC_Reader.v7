f0d751e978ce374f06b0750b0fb11ecbd74c4b4b
Wilconwel <79929040+Wilconwel@users.noreply.github.com>
2021-06-10 19:00:15 -0400

    init

---
+++ constants.py
@@ -0,0 +1,23 @@
+DOTW = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
+MONTHS = ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')
+END_WORKOUT_IDENTIFIER = '-----'
+RELEVANT_DATA_IDENTIFIER = '❍'
+INTENSITY_IDENTIFIER = ('@', 'w/', 'with', 'until')
+MUSCLES = ['Gastrocnemius',
+           'Quadriceps',
+           'Hamstrings',
+           'Adductors',
+           'Abductors',
+           'Gluteal Group',
+           'Spinal Erectors',
+           'Latissimus Dorsi',
+           'Rhomboids',
+           'Trapezius',
+           'Rectus Abdominus',
+           'Obliques',
+           'Pectoralis Group',
+           'Anterior Deltoid',
+           'Lateral Deltoid',
+           'Posterior Deltoid',
+           'Biceps',
+           'Triceps']
---
+++ main.py
@@ -0,0 +1,4 @@
+from classes import WorkoutLog
+
+workout_log = WorkoutLog('resources/Connor Welsh workout log.txt')
+print(workout_log.workouts['21.1.Day_1'].exercises[0])
